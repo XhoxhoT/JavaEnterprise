894ab6c7a21c7a176b8500cdd958138f463defe2
XhoxhoT <xhonitinaj@yahoo.com>
2024-04-09 19:22:59 +0200

    Ndryshimet Xhoni

---
+++ Lab_3/demo/src/main/java/com/example/demo/controller/BookController.java
@@ -0,0 +1,60 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.Book;
+import com.example.demo.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+
+    @Autowired
+    private BookRepository repository;
+
+    @PostMapping
+    public ResponseEntity<?> addBook(@RequestBody Book book) {
+        return new ResponseEntity<>(repository.save(book), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<Collection<Book>> getAllBooks() {
+        return new ResponseEntity<>(repository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> getBookWithId(@PathVariable Long id) {
+        return new ResponseEntity<Book>(repository.findById(id).get(), HttpStatus.OK);
+    }
+
+    @GetMapping(params = {"title"})
+    public ResponseEntity<Collection<Book>> findBookWithName(@RequestParam(value = "title") String title) {
+        return new ResponseEntity<>(repository.findByName(title), HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Book> updateBookFromDB(@PathVariable("id") long id, @RequestBody Book book) {
+
+        Optional<Book> currentBookOpt = repository.findById(id);
+        Book currentBook = currentBookOpt.get();
+        currentBook.setTitle(book.getTitle());
+        currentBook.setAuthor(book.getAuthor());
+
+        return new ResponseEntity<>(repository.save(currentBook), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteBookWithId(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
+
+    @DeleteMapping
+    public void deleteAllBooks() {
+        repository.deleteAll();
+    }
+}
